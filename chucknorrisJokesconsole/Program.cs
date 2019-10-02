a3694de27780675179d9e748ebfad11e84e5670e
shiyeni <ongeziwe@quirky30.co.za>
2019-10-02 15:12:45 +0200

    new text coming

---
+++ chucknorrisJokesconsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using ChckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+
+namespace chucknorrisJokesconsole
+{
+    class chucknorrisJokeconsole
+    {
+        static async Task Main(string[] args)
+
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+            string randomJoke = await jokeGenerator.GetRandomJoke();
+            Console.WriteLine(randomJoke);
+
+            string[] categories = await jokeGenerator.GetCategories();
+            for (int index = 0; index < categories.Length; index++)
+            Console.WriteLine(categories[index]);
+        }
+    }
+}
